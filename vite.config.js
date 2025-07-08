d8db278796fa34709ecd59274462e075ac246f6b
VasilisVas1 <bilakos146@gmail.com>
2025-07-08 13:49:57 +0300

    Server changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/"
+  base: "/",
+  server: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT)||5173      
+  }
 })
